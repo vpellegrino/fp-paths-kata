4c98d6037609a38f4869a0961b4f8d0c3b74dcc8
vpellegr <valentino_pellegrino@bmc.com>
2019-01-24 08:58:45 +0100

    Introduced Lodash + Update test description

--- src/models/Segment.spec.ts
+++ src/models/Segment.spec.ts
@@ -1,20 +1,27 @@
 import {Segment} from "./Segment";
 import {Point} from "./Point";
 
-test('given a segment where endpoints are the same point, then it should return zero as distance between them', () => {
-    const point: Point = new Point(0, 1);
-    const segment: Segment = new Segment(point, point);
+describe('Segment model', () => {
 
-    expect(typeof segment.getDistance).toEqual('function');
-    expect(segment.getDistance()).toEqual(0);
-});
+    describe('given a segment where endpoints are the same point', () => {
+        test('its distance should be zero', () => {
+            const point: Point = new Point(0, 1);
+            const segment: Segment = new Segment(point, point);
 
-test('given a segment where endpoints are not the same point, then it should return a distance between them that is greater than zero', () => {
-    const pointA: Point = new Point(0, 1);
-    const pointB: Point = new Point(0, 5);
-    const segment: Segment = new Segment(pointA, pointB);
+            expect(typeof segment.getDistance).toEqual('function');
+            expect(segment.getDistance()).toEqual(0);
+        });
+    });
 
-    expect(typeof segment.getDistance).toEqual('function');
-    expect(segment.getDistance()).toBeGreaterThan(0);
-    expect(segment.getDistance()).toEqual(4);
+    describe('given a segment where endpoints are not the same point', () => {
+        test('its distance should be greater than zero', () => {
+            const pointA: Point = new Point(0, 1);
+            const pointB: Point = new Point(0, 5);
+            const segment: Segment = new Segment(pointA, pointB);
+
+            expect(typeof segment.getDistance).toEqual('function');
+            expect(segment.getDistance()).toBeGreaterThan(0);
+            expect(segment.getDistance()).toEqual(4);
+        });
+    });
 });
